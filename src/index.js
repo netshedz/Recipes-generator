4d78b16babd2342193c39ae497c58bd2a59fcfeb
netshedzo <67120128+netshedz@users.noreply.github.com>
2024-07-31 04:14:27 +0200

    improved the user experience

--- src/index.js
+++ src/index.js
@@ -14,8 +14,12 @@ function generateRecipes(event) {
   let apiKey = "2f93ebcd2o4334a22af8dt6e0784500a";
   let context =
     "I love to cook . your mission is to generate recipes in basic html.Make sure to follow the user instructions.always list all the recipes.offer readers a range of delicious and practical meal options, provide healthy and easy-to-make dishes. also add ingredients.always separate the recipes and ingredients with a line.Please make sure to include the signature always at the bottom of the page and the signature should be inside <strong>element:Dzhivhuho Netshedzo❤️";
-  let prompt = `Generate recipes ${instructionInput.value}`;
+  let prompt = `user instructions:Generate recipes ${instructionInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#recipe");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating recipes for.. ${instructionInput.value}</div>`;
 
   axios.get(apiUrl).then(displayRecipes);
 }
