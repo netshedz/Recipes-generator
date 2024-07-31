52a60706918d1ec649c520685b18d35d454963cb
netshedzo <67120128+netshedz@users.noreply.github.com>
2024-07-31 03:49:38 +0200

    Added API intergration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-function generateRecipes(event) {
-  event.preventDefault();
-
+function displayRecipes(response) {
   new Typewriter("#recipe", {
-    strings: "chicken stew recipes:chicken, chicken spice, paprika",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateRecipes(event) {
+  event.preventDefault();
+
+  let instructionInput = document.querySelector("#user-instructions");
+  let apiKey = "2f93ebcd2o4334a22af8dt6e0784500a";
+  let context =
+    "I love to cook . your mission is to generate recipes in basic html.Make sure to follow the user instructions.always list all the recipes.offer readers a range of delicious and practical meal options, provide healthy and easy-to-make dishes. also add ingredients.always separate the recipes and ingredients with a line.Please make sure to include the signature always at the bottom of the page and the signature should be inside <strong>element:Dzhivhuho Netshedzo❤️";
+  let prompt = `Generate recipes ${instructionInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayRecipes);
 }
 
 let formElement = document.querySelector("#recipe-generator-form");
